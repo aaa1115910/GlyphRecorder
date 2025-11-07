5884877f2eb6c250a5a74830fe3c7426c838c379
aaa1115910 <aaa1115910@gmail.com>
2025-11-07 23:36:39 +0800

    入侵模拟页面应始终使用深色状态栏

--- app/src/debug/kotlin/dev/aaa1115910/glyphrecorder/activities/HackEmulatorActivity.kt
+++ app/src/debug/kotlin/dev/aaa1115910/glyphrecorder/activities/HackEmulatorActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowInsetsControllerCompat
 import dev.aaa1115910.glyphrecorder.ui.GlyphRecorderTheme
 import dev.aaa1115910.glyphrecorder.ui.screens.HackEmulatorScreen
 
@@ -13,6 +14,7 @@ class HackEmulatorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
         setContent {
             GlyphRecorderTheme(darkTheme = true) {
                 HackEmulatorScreen()
